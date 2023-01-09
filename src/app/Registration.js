54754b5c892adede4839d591b3b10a2d5344fc77
BeniRb <beni.ribakov@gmail.com>
2023-01-09 15:21:10 +0200

    fix register

--- src/app/Registration.js
+++ src/app/Registration.js
@@ -29,8 +29,7 @@ const Registration = () => {
             Password <br></br> <input  placeholder="Enter Password"onChange={(e) => setNewPwd(e.target.value)} type='password' /><br></br>
             Confirm password <br></br> <input placeholder="Confirm Password" onChange={(e) => setNewPwd(e.target.value)} type='password' /><br></br>
             Email <br></br> <input placeholder="Enter Email" onChange={(e) => setconfPWD(e.target.value)} /><br></br>
-            <button onClick={() =>{setTimeout(function() {window.location.replace('https://react-front.onrender.com');}, 2000)
-            ;dispatch(doSignupAsync({ username: newUserName, password: newPwd, email: newEmail }))}}>Register</button>
+            <button onClick={() =>{dispatch(doSignupAsync({ username: newUserName, password: newPwd, email: newEmail }))}}>Register</button>
             </form>
         </div>
   )
